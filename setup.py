89bca30f7488e1e91d37db5a0b0f243c1b51ad1d
ForJadeForest <yingzhepeng@foxmail.com>
2023-06-16 13:58:41 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='otter',
+    version='0.1',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    description='The package of otter for modafew',
+    author='YingzhePeng',
+    author_email='yingzhepeng@foxmail.com',
+    packages=find_packages(),
+)
